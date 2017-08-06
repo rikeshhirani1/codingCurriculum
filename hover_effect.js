55f750f3a0130fa3b311a1d57af32ecbf78479f7
rikeshhirani1 <rikeshhirani1@gmail.com>
2017-08-06 15:15:29 +0100

    Initial commit

---
+++ hover_effect.js
@@ -0,0 +1,138 @@
+//Hover Effect for Tiles with links to projects
+$(document).ready(function () {
+    //HTML/JS TEAM 1 RECTANGLE TILE
+    $(".team_ht1").hover(function () {
+        $(".team_ht_img1").fadeOut();
+        $(".team_ht_txt1").delay(500).fadeIn();
+    }, function () {
+        $(".team_ht_txt1").fadeOut();
+        $(".team_ht_img1").delay(500).fadeIn();
+    });   
+
+    //HTML/JS TEAM 2 RECTANGLE TILE
+    $(".team_ht2").hover(function () {
+        $(".team_ht_img2").fadeOut();
+        $(".team_ht_txt2").delay(500).fadeIn();
+    }, function () {
+        $(".team_ht_txt2").fadeOut();
+        $(".team_ht_img2").delay(500).fadeIn();
+    });   
+
+    //HTML/JS TEAM 3 RECTANGLE TILE
+    $(".team_ht3").hover(function () {
+        $(".team_ht_img3").fadeOut();
+        $(".team_ht_txt3").delay(500).fadeIn();
+    }, function () {
+        $(".team_ht_txt3").fadeOut();
+        $(".team_ht_img3").delay(500).fadeIn();
+    });   
+
+    //HTML/JS TEAM 4 RECTANGLE TILE
+    $(".team_ht4").hover(function () {
+        $(".team_ht_img4").fadeOut();
+        $(".team_ht_txt4").delay(500).fadeIn();
+    }, function () {
+        $(".team_ht_txt4").fadeOut();
+        $(".team_ht_img4").delay(500).fadeIn();
+    }); 
+    
+    //TouchDevelop TEAM 3 RECTANGLE TILE
+    $(".team_td3").hover(function () {
+        $(".team_td_img3").fadeOut();
+        $(".team_td_txt3").delay(500).fadeIn();
+    }, function () {
+        $(".team_td_txt3").fadeOut();
+        $(".team_td_img3").delay(500).fadeIn();
+    });   
+
+    //TouchDevelop TEAM 5 RECTANGLE TILE
+    $(".team_td5").hover(function () {
+        $(".team_td_img5").fadeOut();
+        $(".team_td_txt5").delay(500).fadeIn();
+    }, function () {
+        $(".team_td_txt5").fadeOut();
+        $(".team_td_img5").delay(500).fadeIn();
+    });   
+
+    //TouchDevelop TEAM 6 RECTANGLE TILE
+    $(".team_td6").hover(function () {
+        $(".team_td_img6").fadeOut();
+        $(".team_td_txt6").delay(500).fadeIn();
+    }, function () {
+        $(".team_td_txt6").fadeOut();
+        $(".team_td_img6").delay(500).fadeIn();
+    });   
+
+    //TouchDevelop TEAM 8 RECTANGLE TILE
+    $(".team_td8").hover(function () {
+        $(".team_td_img8").fadeOut();
+        $(".team_td_txt8").delay(500).fadeIn();
+    }, function () {
+        $(".team_td_txt8").fadeOut();
+        $(".team_td_img8").delay(500).fadeIn();
+    });   
+            
+    //Python TEAM 1 RECTANGLE TILE        
+    $(".team_py1").hover(function () {
+        $(".team_py_img1").fadeOut();
+        $(".team_py_txt1").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt1").fadeOut();
+        $(".team_py_img1").delay(500).fadeIn();
+    });   
+
+    //Python TEAM 2 RECTANGLE TILE       
+    $(".team_py2").hover(function () {
+        $(".team_py_img2").fadeOut();
+        $(".team_py_txt2").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt2").fadeOut();
+        $(".team_py_img2").delay(500).fadeIn();
+    });   
+
+    //Python TEAM 3 RECTANGLE TILE       
+    $(".team_py3").hover(function () {
+        $(".team_py_img3").fadeOut();
+        $(".team_py_txt3").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt3").fadeOut();
+        $(".team_py_img3").delay(500).fadeIn();
+    }); 
+    
+    //Python TEAM 4 RECTANGLE TILE         
+    $(".team_py4").hover(function () {
+        $(".team_py_img4").fadeOut();
+        $(".team_py_txt4").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt4").fadeOut();
+        $(".team_py_img4").delay(500).fadeIn();
+    });   
+
+    //Python TEAM 5 RECTANGLE TILE       
+    $(".team_py5").hover(function () {
+        $(".team_py_img5").fadeOut();
+        $(".team_py_txt5").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt5").fadeOut();
+        $(".team_py_img5").delay(500).fadeIn();
+    });   
+
+    //Python TEAM 6 RECTANGLE TILE       
+    $(".team_py6").hover(function () {
+        $(".team_py_img6").fadeOut();
+        $(".team_py_txt6").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt6").fadeOut();
+        $(".team_py_img6").delay(500).fadeIn();
+    });   
+
+    //Python TEAM 7 RECTANGLE TILE       
+    $(".team_py7").hover(function () {
+        $(".team_py_img7").fadeOut();
+        $(".team_py_txt7").delay(500).fadeIn();
+    }, function () {
+        $(".team_py_txt7").fadeOut();
+        $(".team_py_img7").delay(500).fadeIn();
+    });   
+});
+            
